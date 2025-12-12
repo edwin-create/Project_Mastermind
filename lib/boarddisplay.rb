59cd0e8d0ddc88219a0b97eac5b96dc9653fdbfd
edwin-create <edwinwanyoike9999@gmail.com>
2025-12-12 21:52:05 +0300

    Bug fix: Corrected display logic in BoardDisplay Introduced .clear built in ruby method to clear previous guess and feedback entries from the display.

--- lib/boarddisplay.rb
+++ lib/boarddisplay.rb
@@ -11,15 +11,12 @@ class BoardDisplay
   end
 
   def add_turn(guess, feedback)
-    @history.push(guess: guess, feedback: feedback)
+    @history.clear.push(guess: guess, feedback: feedback)
   end
 
   def display
     puts "\nCurrent Board:"
-    @history.each_with_index do |turn, index|
-      guess = turn[:guess].join(', ')
-      feedback = turn[:feedback].map { |f| "Black: #{f[:black]}, White: #{f[:white]}" }.join(' | ')
-      puts "Turn #{index + 1}: Guess: [#{guess}] => Feedback: [#{feedback}]"
-    end
+    puts '-------------------------'
+    puts @history
   end
 end
